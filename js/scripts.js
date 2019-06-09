5242cfea02258cbba26b3cd2f51d122b1e6069f6
henrymutega <hmutega@gmail.com>
2019-06-09 23:18:52 +0300

    Add the show and hide method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
+  $("#designs").hide();
   $(".design").click(function() {
     $("#designs").toggle();
+    $(".design").hide();
   });
+  $("#develops").hide();
   $(".develop").click(function() {
     $("#develops").toggle();
+    $(".develop").hide();
   });
+  $("#products").hide();
   $(".product").click(function() {
     $("#products").toggle();
+    $(".product").hide();
+  });
+});
+$(document).ready(function() {
+  $("#designs").click(function() {
+    $(".design").toggle();
+    $("#designs").hide();
+  });
+  $("#develops").click(function() {
+    $(".develop").toggle();
+    $("#develops").hide();
+  });
+  $("#products").click(function() {
+    $(".product").toggle();
+    $("#products").hide();
   });
 });
