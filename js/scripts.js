55c6dc97c617d3ccb3c77eb93f8b9355b858a806
henrymutega <hmutega@gmail.com>
2019-06-09 22:50:47 +0300

    Add the toggle to the project.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".design").click(function() {
+    $("#designs").toggle();
+  });
+  $(".develop").click(function() {
+    $("#develops").toggle();
+  });
+  $(".product").click(function() {
+    $("#products").toggle();
+  });
+});
